74e54456bb2ed832e5b3c52abc2980cbaff71064
emaknyus <emaknyus@gmail.com>
2016-03-04 10:44:28 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
 VENDOR=$HOME/vendor
-PREFIX=$VENDOR/libjpeg62
+PREFIX=$VENDOR/libtiff
 mkdir -p $PREFIX/bin
 mkdir -p $PREFIX/lib
 mkdir -p $PREFIX/include
 mkdir -p $PREFIX/man/man1
 
-curl -O http://www.ijg.org/files/jpegsrc.v6b.tar.gz
-tar zxf jpegsrc.v6b.tar.gz
-cd jpeg-6b
+# Download tiff dependency
+curl http://download.osgeo.org/libtiff/tiff-4.0.3.tar.gz -o libtiff.tar.gz
+# Unzip
+tar -xvf libtiff.tar.gz
+# Get into libtiff folder
+cd tiff-4.0.3
+
 ./configure --prefix=$PREFIX --enable-shared --enable-static
 make LIBTOOL=libtool
 make install LIBTOOL=libtool
 
 cd $HOME
-tar zcf libjpeg62.tar.gz -C $VENDOR .
+# tar zcf libtiff403.tar.gz -C $VENDOR .
 
 curl https://raw.githubusercontent.com/scottmotte/srvdir-binary/master/srvdir.tar.gz -O
 tar zxf srvdir.tar.gz
